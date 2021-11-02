df53ea11f2dd4afa4e6f1f593a418ea62c488067
SuperNova-94 <johnwinchester21600@gmail.com>
2021-11-02 05:29:42 -0800

    01 - Text was successfully updated in the Formatted Text

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /**
- * TODO: Update the text in the "Formatted Text" section as a user types in the textarea
+ * TODO: Update the text in the "Formatted Text" section as a user types in the textarea ✔
  * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
  * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
  * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
@@ -11,7 +11,8 @@
  * HINT: Use the onkeydown function inside HTML
  */
  function updateText(){
-    // CODE GOES HERE
+    let text = document.getElementById("text-input").value;
+    document.getElementById("text-output").innerText = text;
   }
   
   /**
